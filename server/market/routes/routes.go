2024da3610a328a74798d0b95cf7724e49d776d4
Ales <oxudes@gmail.com>
2016-12-04 15:10:30 +0100

    Clarity
    Slight reorganization

--- server/market/routes/routes.go
+++ server/market/routes/routes.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/moryg/eve_analyst/server/market/routes/overpriced"
 )
 
 func Load(r *httprouter.Router) *httprouter.Router {
-	r.GET("/api/market/overpriced", getOverpriced)
+	r.GET("/api/market/overpriced", overpriced.Get)
 	return r
 }
