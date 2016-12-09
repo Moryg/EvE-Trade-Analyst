f1a8609a8a216a6900a9bd3637d36adb55a5a384
Ales <oxudes@gmail.com>
2016-12-09 21:12:29 +0100

    Changed query type
    Implemented a prepared statement

--- database/market/getOverpricedPage.go
+++ database/market/getOverpricedPage.go
@@ -1,15 +1,21 @@
 package market
 
 import (
+	"github.com/jmoiron/sqlx"
 	. "github.com/moryg/eve_analyst/database"
 	. "github.com/moryg/eve_analyst/shared"
 	"log"
 )
 
-var sql_GetOverPricedPage string
+var getOverPriceStmt *sqlx.Stmt
 
 func init() {
-	sql_GetOverPricedPage = "SELECT `buy`.`stationId`, `buy`.`itemId`, `buy`.`min` `bMin`, `buy`.`mean` `bMean`, `sell`.`min` `sMin`, `sell`.`mean` `sMean`, CAST(`sell`.`min` / `buy`.`min` AS DECIMAL(20,2)) `rMin`, CAST(`sell`.`mean` / `buy`.`mean` AS DECIMAL(20,2)) `rMean` FROM `orderSell` `buy` JOIN `orderSell` `sell` ON `sell`.`stationId` = ? AND `sell`.`itemId` = `buy`.`itemId` WHERE `buy`.`stationID` = ? ORDER BY `rMean` DESC LIMIT ?, ?;"
+	var err error
+	getOverPriceStmt, err = DB.Preparex("SELECT `buy`.`stationId`, `buy`.`itemId`, `buy`.`min` `bMin`, `buy`.`mean` `bMean`, `sell`.`min` `sMin`, `sell`.`mean` `sMean`, CAST(`sell`.`min` / `buy`.`min` AS DECIMAL(20,2)) `rMin`, CAST(`sell`.`mean` / `buy`.`mean` AS DECIMAL(20,2)) `rMean` FROM `orderSell` `buy` JOIN `orderSell` `sell` ON `sell`.`stationId` = ? AND `sell`.`itemId` = `buy`.`itemId` WHERE `buy`.`stationID` = ? ORDER BY `rMean` DESC LIMIT ?, ?;")
+
+	if err != nil {
+		log.Fatal("Failed preparing getOverPriced query statement")
+	}
 }
 
 func GetOverPricedPage(buyId, sellId int64, page int) []CompItem {
@@ -21,7 +27,7 @@ func GetOverPricedPage(buyId, sellId int64, page int) []CompItem {
 		page = 0
 	}
 	page = page * perPage
-	err := DB.Select(&items, sql_GetOverPricedPage, buyId, sellId, page, perPage)
+	err := getOverPriceStmt.Select(&items, buyId, sellId, page, perPage)
 	if err != nil {
 		log.Printf("getOverpriced query: " + err.Error())
 	}
